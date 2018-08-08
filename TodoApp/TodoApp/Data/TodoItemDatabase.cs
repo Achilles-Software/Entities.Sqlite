b77cb443d5f630491f56eee66675f854edf23541
ToddThomson <achilles@telus.net>
2018-08-08 10:37:06 -0700

    sample updated. Prerelease note in TodoItemDatabase.cs

--- TodoApp/TodoApp/Data/TodoItemDatabase.cs
+++ TodoApp/TodoApp/Data/TodoItemDatabase.cs
@@ -8,49 +8,51 @@ using System.Threading.Tasks;
 namespace TodoApp
 {
     public class TodoItemDatabase
-	{
-		readonly TodoDbContext _dbContext;
+    {
+        readonly TodoDbContext _dbContext;
 
-		public TodoItemDatabase(string dbPath)
-		{
+        public TodoItemDatabase( string dbPath )
+        {
             var connectionString = "Data Source=" + dbPath;
             var options = new DbContextOptionsBuilder().UseSqlite( connectionString ).Options;
 
             _dbContext = new TodoDbContext( options );
 
             _dbContext.Database.Creator.CreateIfNotExists();
-		}
+        }
 
-		public Task<List<TodoItem>> GetItemsAsync()
-		{
-			return _dbContext.TodoItems.ToListAsync();
-		}
+        public Task<List<TodoItem>> GetItemsAsync()
+        {
+            // TJT: Select( i => i ) is required as a workaround for a prerelease bug.
+            return _dbContext.TodoItems.Select( i => i ).ToListAsync();
+        }
 
-		public Task<List<TodoItem>> GetItemsNotDoneAsync()
-		{
+        public Task<List<TodoItem>> GetItemsNotDoneAsync()
+        {
+            // TJT: Select( i => i ) is required as a workaround for a prerelease bug.
             return _dbContext.TodoItems.Select( i => i ).Where( i => i.Done == false ).ToListAsync();
-		}
+        }
 
-		public Task<TodoItem> GetItemAsync(int id)
-		{
+        public Task<TodoItem> GetItemAsync( int id )
+        {
             return _dbContext.TodoItems.FirstAsync( p => p.Id == 1 );
         }
 
-		public Task<int> SaveItemAsync(TodoItem item)
-		{
-			if (item.Id != 0)
-			{
-				return _dbContext.TodoItems.UpdateAsync(item);
-			}
-			else {
-				return _dbContext.TodoItems.AddAsync(item);
-			}
-		}
+        public Task<int> SaveItemAsync( TodoItem item )
+        {
+            if ( item.Id != 0 )
+            {
+                return _dbContext.TodoItems.UpdateAsync( item );
+            }
+            else
+            {
+                return _dbContext.TodoItems.AddAsync( item );
+            }
+        }
 
-		public Task<int> DeleteItemAsync(TodoItem item)
-		{
-			return _dbContext.TodoItems.DeleteAsync(item);
-		}
-	}
+        public Task<int> DeleteItemAsync( TodoItem item )
+        {
+            return _dbContext.TodoItems.DeleteAsync( item );
+        }
+    }
 }
-
